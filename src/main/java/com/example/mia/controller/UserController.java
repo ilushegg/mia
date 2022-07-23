9c0f7a58b8a97856c3af07fb620336de3ca082ae
ilushegg <marnopolskiyi@mail.ru>
2022-07-23 10:57:14 +0300

    Added the ability to edit a user role

--- src/main/java/com/example/mia/controller/UserController.java
+++ src/main/java/com/example/mia/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.example.mia.controller;
 
+import com.example.mia.domain.Role;
+import com.example.mia.domain.User;
 import com.example.mia.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 
 @Controller
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     private final UserService userService;
@@ -17,12 +25,30 @@ public class UserController {
         this.userService = userService;
     }
 
-
-    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/users")
     public String userList(Model model){
         model.addAttribute("users", userService.findAll());
         return "userList";
     }
+
+    @GetMapping("/users/{user}")
+    public String userEditForm(@PathVariable User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping("/users")
+    public String userEditSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user){
+        userService.saveUser(user, username, form);
+        return "redirect:users";
+    }
+
+
+
+
 
 }
--- src/main/java/com/example/mia/service/UserService.java
+++ src/main/java/com/example/mia/service/UserService.java
@@ -1,13 +1,16 @@
 package com.example.mia.service;
 
+import com.example.mia.domain.Role;
 import com.example.mia.domain.User;
 import com.example.mia.repos.UserRepo;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -26,5 +29,20 @@ public class UserService implements UserDetailsService {
 
     public List<User> findAll(){
         return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for(String key : form.keySet()){
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
     }
 }
